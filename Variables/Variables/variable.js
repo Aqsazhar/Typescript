34973821e773b1233d6ce41d71088ead305264d8
AqsaAzhar <167401580+Aqsazhar@users.noreply.github.com>
2024-06-26 22:24:22 +0500

    Add files via upload

---
+++ Variables/Variables/variable.js
@@ -0,0 +1,22 @@
+"use strict";
+//VARIABLE
+// let Fname = "Aqsa";
+// let Lname = "Azhar";
+// let age = 25;
+// console.log(Fname,Lname,age);
+//SPECIAL SINGN( $, _ , NUMBER )
+let First_Name = "Aqsa_Azhar";
+let $sir = "Bilal";
+let _School = "GAI";
+let students5 = 100;
+//TEMPLATE LITERALS
+console.log(`${First_Name} ${$sir} ${_School} ${students5}`);
+//CONST ( same varaible not Declaird more time ) 
+const colour = "Red";
+const box = "colour box";
+console.log(colour + " " + box);
+//PREMITIVE DATA TYPES
+let nbox = 20;
+let myName = "Aqsa";
+let isold = true;
+console.log(nbox, myName, isold);
---
+++ Variables/Variables/variable.ts
@@ -0,0 +1,33 @@
+//VARIABLE
+
+let Fname = "Aqsa";
+let Lname = "Azhar";
+let age = 25;
+console.log(Fname,Lname,age);
+
+//SPECIAL SINGN( $, _ , NUMBER )
+
+let First_Name ="Aqsa_Azhar";
+let $sir = "Bilal";
+let _School = "GAI";
+let students5 = 100;
+
+//TEMPLATE LITERALS
+
+console.log(`${First_Name} ${$sir} ${_School} ${students}`)
+
+
+//CONST ( same varaible not Declaird more time ) 
+
+const colour = "Red";
+const box = "colour box";
+console.log(colour + " " + box)
+
+
+//PREMITIVE DATA TYPES
+
+
+let nbox:number =  20;
+let myName:string = "Aqsa";
+let isold:boolean = true;
+console.log(nbox,myName,isold)
