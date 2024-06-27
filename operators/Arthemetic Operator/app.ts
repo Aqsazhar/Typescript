35480792ccf12fc4616507f951e8f6f41e91f6da
AqsaAzhar <167401580+Aqsazhar@users.noreply.github.com>
2024-06-27 20:48:27 +0500

    Add files via upload

---
+++ operators/Arthemetic Operator/app.ts
@@ -0,0 +1,60 @@
+
+ARTHEMETIC OPERATOR
+
+ADDITION
+
+let n1 = 2
+let n2 = 3
+let value1 = "hy";
+let value2 = "hellow";
+console.log(value1 +" "+ value2)
+console.log(`${n1+n2}  ${value1+value2}`)
+
+SUBTRACTION
+
+let num1 = 9
+let num2 = 4
+console.log(num1 - num2)
+
+MULTIPICATION
+
+let number1 = 2
+let number2 = 2
+console.log(number1*number2)
+ 
+DIVISION
+
+let numb1 = 4
+let numb2 = 2
+console.log(numb1 / numb2)
+
+MODULUS
+
+let power1 = 6
+let power2 = 3
+console.log(power1 % power2)
+
+COMPARISON OPERATOR , ASSIGNMENT OPERATOR
+
+EQUAL
+
+let a = 5
+console.log(a === 5) equall
+let b = 3
+console.log(b+=3) 1st method of add
+let c = 2
+console.log(c+c) 2nd method of add
+let d = 4
+console.log(d!=4) not equall (!)
+console.log(d > 4)
+console.log(d < 4)
+console.log(3>5) 3 greater then 5
+console.log(3<5) 3 less then 5
+console.log(d >= 4) chota hy ya brabr hy agr dono me sy ak b true hogi to wo dy dyga
+
+LOGICAL OPERATOR && ||
+
+let l = 5;
+console.log(l > 5 && l <= 5)
+console.log(l >= 5 && l < 5)
+console.log(l < 5 || l >= 5);
